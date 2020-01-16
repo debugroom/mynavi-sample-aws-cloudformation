5946950c6a02bc918b5cf1f04e2b497ccedecade
Kohei Kawabata <kohei.kawabata@mac.com>
2020-01-17 03:44:24 +0900

    Add security group , nat-gateway, alb, targetgroup cloudformation tempalte.

--- delete-stack.sh
+++ delete-stack.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-stack_name="mynavi-sample-vpc"
+#stack_name="mynavi-sample-tg-frontend-webapp"
+#stack_name="mynavi-sample-tg-backend-serviceA"
+#stack_name="mynavi-sample-tg-backend-serviceB"
+stack_name="mynavi-sample-alb"
+#stack_name="mynavi-sample-ng"
+#stack_name="mynavi-sample-sg"
+#stack_name="mynavi-sample-vpc"
 
 aws cloudformation delete-stack --stack-name ${stack_name}
