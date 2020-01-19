83881cab7f604265e8b34c52313c666bd0b2d8aa
Kohei Kawabata <kohei.kawabata@mac.com>
2020-01-19 15:28:05 +0900

    Add rds cloudformation tempalte.

--- delete-stack.sh
+++ delete-stack.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
+stack_name="mynavi-sample-rds"
 #stack_name="mynavi-sample-tg-frontend-webapp"
 #stack_name="mynavi-sample-tg-backend-serviceA"
 #stack_name="mynavi-sample-tg-backend-serviceB"
-stack_name="mynavi-sample-alb"
+#stack_name="mynavi-sample-alb"
 #stack_name="mynavi-sample-ng"
 #stack_name="mynavi-sample-sg"
 #stack_name="mynavi-sample-vpc"
