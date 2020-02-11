9978945ecc5d23fc83c7aacb3f5c3181ca0196e3
kohei.kawabata <kohei.kawabata@mac.com>
2020-02-12 06:59:40 +0900

    Fix miscorrected alb dns builder and add production config.

--- frontend-webapp/src/main/java/org/debugroom/mynavi/sample/cloudformation/frontend/config/ProductionConfig.java
+++ frontend-webapp/src/main/java/org/debugroom/mynavi/sample/cloudformation/frontend/config/ProductionConfig.java
@@ -1,53 +1,39 @@
 package org.debugroom.mynavi.sample.cloudformation.frontend.config;
 
 import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.services.cloudformation.AmazonCloudFormationClient;
-import com.amazonaws.services.cloudformation.model.Export;
-import com.amazonaws.services.cloudformation.model.ListExportsRequest;
-import com.amazonaws.services.cloudformation.model.ListExportsResult;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import org.debugroom.mynavi.sample.cloudformation.common.apinfra.cloud.aws.CloudFormationStackInfo;
 import org.debugroom.mynavi.sample.cloudformation.common.apinfra.cloud.aws.S3Info;
 import org.debugroom.mynavi.sample.cloudformation.common.apinfra.cloud.aws.SQSInfo;
 import org.debugroom.mynavi.sample.cloudformation.common.apinfra.log.interceptor.MDCLoggingInterceptor;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.aws.cache.config.annotation.CacheClusterConfig;
-import org.springframework.cloud.aws.cache.config.annotation.EnableElastiCache;
-import org.springframework.cloud.aws.context.config.annotation.EnableStackConfiguration;
-import org.springframework.cloud.aws.core.env.ResourceIdResolver;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.web.client.RestOperations;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-@Profile("staging")
-@EnableStackConfiguration(stackName = "mynavi-sample-infra-staging")
+@Profile("production")
 // @EnableElastiCache only For Cache, spring data session does not support.
 // @EnableElastiCache({@CacheClusterConfig(name = "ElastiCacheRedis")})
 @Configuration
-public class StagingConfig {
+public class ProductionConfig {
 
-    private static final String PROTOCOL = "http://";
-    private static final String ALB_STACK_NAME = "ALBStagingStack";
-    private static final String ALB_BACKEND_ALB_EXPORT_NAME = "mynavi-sample-cloudformation-vpc-BackendALBDNS-Staging";
-    private static final String S3_STACK_NAME = "S3StagingStack";
-    private static final String S3_BUCKET_EXPORT = "MynaviSampleS3Bucket-Staging";
-    private static final String SQS_STACK_NAME = "SQSStagingStack";
-    private static final String SQS_QUEUE_EXPORT = "MynaviSampleSQS-Staging";
-    private static final String SQS_ENDPOINT_EXPORT = "MynaviSampleSQS-Staging-ServiceEndpoint";
-    private static final String SQS_REGION_EXPORT = "MynaviSampleSQS-Staging-Region";
-    private static final String ELASTICACHE_STACK_NAME = "ElastiCacheStagingStack";
-    private static final String ELASTICACHE_ENDPOINT_EXPORT = "mynavi-sample-cloudformation-vpc-ElastiCacheRedisEndPoint-Staging";
-    private static final String ELASTICACHE_PORT_EXPORT = "mynavi-sample-cloudformation-vpc-ElastiCacheRedisPort-Staging";
+    private static final String PROTOCOL = "https://";
+    private static final String ALB_STACK_NAME = "ALBProductionStack";
+    private static final String ALB_BACKEND_ALB_EXPORT_NAME = "mynavi-sample-cloudformation-vpc-BackendALBDNS-Production";
+    private static final String S3_STACK_NAME = "S3ProductionStack";
+    private static final String S3_BUCKET_EXPORT = "MynaviSampleS3Bucket-Production";
+    private static final String SQS_STACK_NAME = "SQSProductionStack";
+    private static final String SQS_QUEUE_EXPORT = "MynaviSampleSQS-Production";
+    private static final String SQS_ENDPOINT_EXPORT = "MynaviSampleSQS-Production-ServiceEndpoint";
+    private static final String SQS_REGION_EXPORT = "MynaviSampleSQS-Production-Region";
+    private static final String ELASTICACHE_STACK_NAME = "ElastiCacheProductionStack";
+    private static final String ELASTICACHE_ENDPOINT_EXPORT = "mynavi-sample-cloudformation-vpc-ElastiCacheRedisEndPoint-Production";
+    private static final String ELASTICACHE_PORT_EXPORT = "mynavi-sample-cloudformation-vpc-ElastiCacheRedisPort-Production";
 
     @Bean
     CloudFormationStackInfo cloudFormationStackInfo(){
@@ -57,7 +43,7 @@ public class StagingConfig {
     @Bean
     public RestOperations restOperations(RestTemplateBuilder restTemplateBuilder){
         return restTemplateBuilder.rootUri(PROTOCOL + cloudFormationStackInfo().getExportValue(
-              ALB_STACK_NAME , ALB_BACKEND_ALB_EXPORT_NAME))
+                ALB_STACK_NAME , ALB_BACKEND_ALB_EXPORT_NAME))
                 .interceptors(new MDCLoggingInterceptor()).build();
     }
 
